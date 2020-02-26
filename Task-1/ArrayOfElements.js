2ddf99b420c37432cf9fba087480b3071e97ae1e
sdinesh999 <saidinesh949@gmail.com>
2020-02-26 16:04:37 +0530

    Added files

---
+++ Task-1/ArrayOfElements.js
@@ -0,0 +1,12 @@
+1. Input: Array of elements
+["h","o","l","a"]
+Output: String with comma delimited elements of the array in th same order.
+"h,o,l,a".
+
+
+
+//program
+
+function printArray(array){
+ return array.toString()
+}
