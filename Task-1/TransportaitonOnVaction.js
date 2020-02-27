05272a15a124b38a97f0888e973204b9eeb14f88
sdinesh999 <saidinesh949@gmail.com>
2020-02-27 12:03:01 +0530

    delete master branch

--- Task-1/TransportaitonOnVaction.js
+++
@@ -1,5 +0,0 @@
-function rentalCarCost(d) {  
-  var day = d >= 3 && d < 7 ? 20 : 0, 
-      week = d >= 7 ? 50 : 0; 
-return (d * 40) - day - week;
-}
--- Task-2/WordCount.js
+++
@@ -1,6 +0,0 @@
-function countWords(str) {
-    if(str.trim()==""){
-    return 0;
-    }
-    return str.trim().split(/\s+/).length;
-  }
