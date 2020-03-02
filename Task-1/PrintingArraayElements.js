80090fa2cd1ba67c1f44ffdcbbb35a812d3e092d
sdinesh999 <35901397+sdinesh999@users.noreply.github.com>
2020-03-02 16:51:11 +0530

    Add files via upload

---
+++ Task-1/PrintingArraayElements.js
@@ -0,0 +1,10 @@
+1.Input: Array of elements
+["h","o","l","a"]
+Output: String with comma delimited elements of the array in th same order.
+"h,o,l,a"
+
+
+//solution
+function printArray(str){
+ return str.toString();
+}
